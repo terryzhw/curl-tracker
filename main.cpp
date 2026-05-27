1cac696042eea23696bd6e99e809d1874eb400e0
Terrance Wong <62264266+terryzhw@users.noreply.github.com>
2026-05-27 14:17:28 -0700

    Enhance motion detection and feedback mechanisms
    Refactor angle calculations and add rep/set detection.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <MPU6050.h>
 #include <U8g2lib.h>
 
-// SSD1306 128x64 OLED via I2C (SDA=21, SCL=22)
+// SSD1306 128x64 OLED via I2C, SDA=21, SCL=22
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
 
 MPU6050 mpu;
@@ -16,19 +16,46 @@ MPU6050 mpu;
 
 unsigned long prevTime = 0;
 const float ALPHA = 0.90;
-float degrees = 100.0;
+float angle = 0.0f;
+float baselineAngle = 0.0f;
+
+const float UP_OFFSET = 40.0f;
+const float DOWN_OFFSET = 15.0f;
+
 int rep = 0; 
+int set = 0;
 
 enum ArmState {
     ARM_DOWN,
     ARM_UP
 };
 
-// Vibrate at given strength (0=off, 255=full) for durationMs
+ArmState armState = ARM_DOWN;
+
+// Vibrate at given strength 0=off, 255-full for durationMs
 void vibrate(uint8_t strength, uint32_t durationMs) {
     ledcWrite(MOTOR_CH, strength);
     delay(durationMs);
     ledcWrite(MOTOR_CH, 0);
+}
+
+void detectRep(const float upThresh, const float downThresh) {
+    if (armState == ARM_DOWN && angle > upThresh) {
+        armState = ARM_UP;
+    } else if (armState == ARM_UP && angle < downThresh) {
+     armState = ARM_DOWN;
+     rep++;
+     vibrate(220, 100);
+    }
+}
+
+void countSets() {
+    if (rep >= 8) {
+        set++;
+        vibrate(220,100);
+        delay(100);
+        vibrate(220,100);
+    }
 }
 
 
@@ -55,6 +82,25 @@ void setup() {
         while (true);
     }
 
+    prevTime = micros();
+    for (int i=0; i<200; i++) {
+        int16_t ax, ay, az, gx, gy, gz;
+        mpu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+        // Convert raw to g and degs
+        float axG = ax / 16384.0f;
+        float azG = az / 16384.0f;
+        float gyD = gy / 131.0f;
+        float accelPitch = atan2(axG, azG) * (180.0f/PI);
+        unsigned long currTime = micros();
+        float dt = (currTime - prevTime) / 1000000.0f;
+        prevTime = currTime;
+        angle = (ALPHA * (angle + gyD * dt) + (1-ALPHA) * accelPitch);
+        delay(5);
+    }
+
+    baselineAngle = angle;
+
 }
 
 void loop() {
@@ -66,25 +112,33 @@ void loop() {
     float ayG = ay / 16384.0f;
     float azG = az / 16384.0f;
     float gyD = gy / 131.0f;
-
     // Complementary filter
     float accelPitch = atan2(axG, azG) * (180.0f/PI);
     unsigned long currTime = micros();
     float dt = (currTime - prevTime) / 1000000.0f;
     prevTime = currTime;
-    degrees = (ALPHA * (degrees + gyD * dt) + (1-ALPHA) * accelPitch);
+    angle = (ALPHA * (angle + gyD * dt) + (1-ALPHA) * accelPitch);
 
     char buf[32];
     u8g2.clearBuffer();
-    u8g2.drawStr(2, 24, "Degrees: ");
-    snprintf(buf, sizeof(buf), "%.1f", degrees);
+    u8g2.drawStr(2, 24, "Reps: ");
+    snprintf(buf, sizeof(buf), "%.1f", angle);
     u8g2.drawStr(2, 34, buf);
     u8g2.sendBuffer();
 
     // magnitude of accelerometer 
-    float speed = sqrt(axG*axG + ayG*ayG + azG*azG);                                                  
-    if (speed > 2.0f) {                                                                               
-      vibrate(220, 80); 
-    }
+    // float speed = sqrt(axG*axG + ayG*ayG + azG*azG);                                                  
+    // if (speed > 2.0f) {                                                                               
+    //   vibrate(220, 80); 
+    // }
 
-}
+    float upThresh   = baselineAngle + UP_OFFSET;
+    float downThresh = baselineAngle + DOWN_OFFSET;
+
+    detectRep(upThresh, downThresh);
+    countSets();
+
+
+ }
+
+
