9eb4daf3eba9463079392c08030b88cfe64b1e00
Terrance Wong <twongzhw@gmail.com>
2026-07-06 15:59:44 -0700

    added data collection script + integration with main.cpp + better commenting

--- src/main.cpp
+++ src/main.cpp
@@ -23,7 +23,7 @@
 
 static constexpr float ALPHA = 0.90f;
 
-// Sample rate: 1000 / (1 + SRD) Hz. SRD=9 -> 100 Hz.
+// IMU samples at 100 times per second which is more than enough for bicep curl (Original 1000Hz overkill)
 static constexpr uint8_t SAMPLE_RATE_DIV = 9;
 static constexpr float DT = (1.0f + SAMPLE_RATE_DIV) / 1000.0f;  
 
@@ -31,13 +31,15 @@ bfs::Mpu6500 mpu6500(&Wire, bfs::Mpu6500::I2C_ADDR_PRIM);
 U8X8_SSD1306_128X64_NONAME_HW_I2C oled(U8X8_PIN_NONE);
 
 float roll = 0.0f;
+float pitch = 0.0f;
+
 bool filterInitialized = false;
+bool collecting = false;
 
 int reps = 0;
 int sets = 0;
 
 int isPressed = 1;
-
 
 
 
@@ -55,6 +57,8 @@ void taskSampleIMU(void *pvParameters) {
   mpu6500.ConfigDlpfBandwidth(bfs::Mpu6500::DLPF_BANDWIDTH_20HZ);
   mpu6500.ConfigSrd(SAMPLE_RATE_DIV);
 
+  Serial.println("timestamp_ms,roll,label");
+
   for (;;) {
     if (mpu6500.Read()) {
       float ax = mpu6500.accel_x_mps2();
@@ -62,21 +66,36 @@ void taskSampleIMU(void *pvParameters) {
       float az = mpu6500.accel_z_mps2();
 
       float gx = mpu6500.gyro_x_radps();
+      float gy = mpu6500.gyro_y_radps();
+      float gz = mpu6500.gyro_z_radps();
 
-      float accelRoll = atan2f(ay, -az) * (180.0f / M_PI);
+      // Complementary filter; no yaw since not needed for bicep curls and no magnetometer to prevent drift
+
+      float accelRoll = atan2f(ay, az) * (180.0f / M_PI);
+      float accelPitch = atan2f(ax, sqrtf(sq(ay) + sq(az))) * (180.0f / M_PI);
 
       if (!filterInitialized) {
         roll = accelRoll;
+        pitch = accelPitch;
         filterInitialized = true;
       } else {
-        // Complementary filter
         roll = ALPHA * (roll + gx * (180.0f / M_PI) * DT)
              + (1.0f - ALPHA) * accelRoll;
+        pitch = ALPHA * (pitch + gy * (180.0f / M_PI) * DT)
+              + (1.0f - ALPHA) * accelPitch;
       }
 
-      Serial.print("Roll: ");
-      Serial.print(roll, 1);
-      Serial.println(" deg");
+      // collect.py reads this to write to data.csv
+      if (collecting) {
+        int label = (isPressed == LOW) ? 1 : 0;
+        Serial.print(millis());
+        Serial.print(",");
+        Serial.print(roll, 2);
+        Serial.print(",");
+        Serial.print(pitch, 2);
+        Serial.print(",");
+        Serial.println(label);
+      }
     }
 
     vTaskDelay(pdMS_TO_TICKS(10));
@@ -107,11 +126,6 @@ void taskDisplayOLED(void *pvParameters) {
     oled.print(sets, 1);
 
     oled.setCursor(0,6);
-    oled.print("ANGLE:");
-    oled.setCursor(6,6);
-    oled.print(roll, 1);
-
-    oled.setCursor(9,2);
     oled.print("MQTT:OK");
 
     vTaskDelay(pdMS_TO_TICKS(50));
@@ -137,6 +151,7 @@ void taskSampleButton(void *pvParameters) {
   }
 }
 
+// Haptic feedback for curls
 void taskVibrateMotor(void *pvParamters) {
 
   ledcSetup(motorChannel, motorFrequency, motorResolution);
@@ -153,6 +168,24 @@ void taskVibrateMotor(void *pvParamters) {
   }
 }
 
+// Toggle for data collection
+void taskSerialControl(void *pvParameters) {
+  for (;;) {
+    if (Serial.available()) {
+      char c = Serial.read();
+      if (c == 's') {
+        collecting = !collecting;
+        if (collecting) {
+          Serial.println("Collecting start");
+        } else {
+          Serial.println("Collecting stop");
+        }
+      }
+    }
+    vTaskDelay(pdMS_TO_TICKS(50));
+  }
+}
+
 void setup() {
   Serial.begin(9600);
   Wire.begin(I2C_SDA, I2C_SCL);
@@ -161,6 +194,7 @@ void setup() {
   xTaskCreate(taskDisplayOLED, "displayOLED", 2048, NULL, 1, NULL);
   xTaskCreate(taskSampleButton, "sampleButton", 2048, NULL, 1, NULL);
   xTaskCreate(taskVibrateMotor, "vibrateMotor", 2048, NULL, 1, NULL);
+  xTaskCreate(taskSerialControl, "serialCtrl", 2048, NULL, 1, NULL);
 }
 
 void loop() {
